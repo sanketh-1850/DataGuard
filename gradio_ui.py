6984f1f62ede657b21517cb4f75180568edb68bd
Sanketh <sankethsequeira@gmail.com>
2025-02-01 08:13:08 +0530

    major UI changes and added gemini

--- gradio_ui.py
+++ gradio_ui.py
@@ -1,13 +1,39 @@
+import os
 import gradio as gr
 import masking
 import re
+import google.generativeai as genai
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+
+genai.configure(api_key = os.getenv('GEMINI_API_KEY'))
+
+model = genai.GenerativeModel('gemini-2.0-flash-exp')
+
+custom_button = """
+<style>
+    #gemini_submit {
+        margin: 0.2em 0em 0.2em 0;
+        max-width: 4.0em;
+        min-width: 4.0em !important;
+        height: 3.6em;
+        background-image: url('https://img.icons8.com/material-outlined/24/arrow.png');
+        background-size: cover;
+        background-repeat: no-repeat;
+        border: none;
+        background-position: center center;
+        background-size: 30px;
+    }
+</style>
+"""
 
 custom_head = """
 <iframe style="display:none" onload="
     document.title = 'DataGuard';
     var link = document.querySelector('link[rel=icon]') || document.createElement('link');
     link.rel = 'icon';
-    link.href = 'https://cdn-icons-png.flaticon.com/512/6356/6356296.png';  // Replace with your favicon URL
+    link.href = 'https://cdn-icons-png.flaticon.com/512/6356/6356296.png';
     document.head.appendChild(link);
 "></iframe>
 """
@@ -21,7 +47,7 @@ custom_title = """
     <p style="color: White; text-align: center; font-size: 20px; margin-top: 10px;">
         Protecting Privacy, Empowering Data.
     </p>
-    <hr style="padding-bottom: 40px;">
+    <hr style="padding-bottom: 5px;">
 </div>
 """
 
@@ -30,6 +56,39 @@ custom_footer = """
     footer {display: none !important;}
 </style>
 """
+
+def handle_user_query(msg, chatbot):
+    print(msg, chatbot)
+    if(not (msg.isspace() or msg == '')):
+        chatbot += [[msg, None]]
+    return ('', chatbot)
+
+def handle_gemini_response(chatbot):
+    query = chatbot[-1][0]
+    formatted_chatbot = generate_chatbot(chatbot[:-1])
+    chat = model.start_chat(history = formatted_chatbot)
+    response = chat.send_message(query)
+    chatbot[-1][1] = response.text
+    return (chatbot)
+
+def generate_chatbot(chatbot: list[list[str, str]]) -> list[list[str, str]]:
+    formatted_chatbot = []
+    if len(chatbot) == 0:
+        return formatted_chatbot
+    for ch in chatbot:
+        formatted_chatbot.append(
+            {
+                "role": "user",
+                "parts": [ch[0]]
+            }
+        )
+        formatted_chatbot.append(
+            {
+                "role": "model",
+                "parts": [ch[1]]
+            }
+        )
+    return formatted_chatbot
 
 
 def process_input(inp: str) -> str:
@@ -45,18 +104,50 @@ def clear_function() -> tuple:
     return '', ''
 
 def gradio_interface():
-    with gr.Blocks(theme = gr.themes.Soft(primary_hue="cyan"), fill_width = True) as interface:
+    with gr.Blocks(theme = gr.themes.Soft(primary_hue="cyan"), fill_width = False) as interface:
         gr.HTML(custom_head)
         gr.HTML(custom_title)
         gr.HTML(custom_footer)
-        with gr.Column():
+        gr.HTML(custom_button)
+        with gr.Tab(label = 'Masking-Only'):
+            with gr.Column():
+                with gr.Row():
+                    input_txt = gr.components.Textbox(label = 'Raw Input', placeholder = "Enter text here...", autofocus = True, lines = 20)
+                    output_txt = gr.components.Textbox(label = 'Masked Output', show_copy_button = True, lines =20)
+                with gr.Row():
+                    submit = gr.components.Button(value = 'Submit', variant = 'primary')
+                    clear = gr.components.Button(value = 'Clear', variant = 'stop')
+            submit.click(process_input, [input_txt], [output_txt])
+            clear.click(clear_function, [], [input_txt, output_txt])
+        with gr.Tab(label = 'Masked-Gemini'):
+            chatbot = gr.Chatbot(
+                label = 'Chat with Gemini',
+                bubble_full_width = False,
+                show_copy_button = True,
+                show_label = False,
+            )
             with gr.Row():
-                input_txt = gr.components.Textbox(label = 'Raw Input', placeholder = "Enter text here...", autofocus = True, lines = 20)
-                output_txt = gr.components.Textbox(label = 'Masked Output', show_copy_button = True, lines =20)
-            with gr.Row():
-                submit = gr.components.Button(value = 'Submit', variant = 'primary')
-                clear = gr.components.Button(value = 'Clear', variant = 'stop')
-        submit.click(process_input, [input_txt], [output_txt])
-        clear.click(clear_function, [], [input_txt, output_txt])
+                msg = gr.Textbox(show_label = False, elem_id = 'gemini_text')
+                submit = gr.components.Button(value = '', variant = 'primary', elem_id = 'gemini_submit')
+            clear = gr.ClearButton([msg, chatbot])
+            msg.submit(
+                handle_user_query,
+                [msg, chatbot],
+                [msg, chatbot]
+            ).then(
+                handle_gemini_response,
+                [chatbot],
+                [chatbot]
+            )
 
-    interface.launch(share = False, show_api = False)
+            submit.click(
+                handle_user_query,
+                [msg, chatbot],
+                [msg, chatbot]
+            ).then(
+                handle_gemini_response,
+                [chatbot],
+                [chatbot]
+            )
+
+    interface.launch(share = True, show_api = False)
